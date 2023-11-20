a94ae1761e91cde9d1b254bd61b83cc6127278b6
faizal108 <69467378+faizal108@users.noreply.github.com>
2023-11-20 19:18:25 +0530

    Security Added

---
+++ pharmacy/src/main/java/com/meditrack/pharmacy/auth/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.meditrack.pharmacy.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    String password;
+}
